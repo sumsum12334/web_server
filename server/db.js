ddd2660a03daeb1875f5b65f3ec5477ce8a3a8c1
sumsum12334 <87108541+sumsum12334@users.noreply.github.com>
2021-11-13 23:31:37 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool =  mysql.createPool({
-    user: "root",
-    password: "QQLyew67",
+    user: "admin",
+    password: "wsleung329",
     port: 3306,
-    database: "EE4221",
-    host: "localhost"
+    database: "proj1",
+    host: "projdb1.cuklflnkaq3u.us-east-1.rds.amazonaws.com"
 });
 
 module.exports = pool;
